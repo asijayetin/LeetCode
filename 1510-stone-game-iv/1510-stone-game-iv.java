4f604fa599599d9b86f3116ded296829ce056169
asijayetin <asijayetin@gmail.com>
2026-08-10 12:10:54 +0530

    Time: 356 ms (5.22%), Space: 78.8 MB (5.48%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,16 @@
+class Solution {
+    HashMap<Integer,Boolean> map=new HashMap<>();
+    public boolean winnerSquareGame(int n) {
+        if (n==0) return false;
+
+        if (map.containsKey(n)) return map.get(n);
+        for (int i=1;i*i<=n;i++) {
+            if (!winnerSquareGame(n-i*i)){
+                map.put(n,true);
+                return true;
+            }
+        }
+        map.put(n,false);
+        return false;
+    }
+}
