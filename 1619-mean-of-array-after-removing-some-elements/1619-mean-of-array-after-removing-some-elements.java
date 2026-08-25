491c873888776014e8c33f8a2db55259992921d0
asijayetin <asijayetin@gmail.com>
2026-08-25 12:32:13 +0530

    Time: 6 ms (86.49%), Space: 47.2 MB (77.18%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int sum=0;
+        int remove=n/20;
+        for(int i=remove;i<n-remove;i++){
+            sum+=arr[i];
+        }
+        return (double) sum/(n-2*remove);
+
+    }
+}
