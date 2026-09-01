8e2d1ea94f42eeb5fddf31d6994ca17130d1c12e
asijayetin <asijayetin@gmail.com>
2026-09-01 13:27:18 +0530

    Time: 0 ms (100%), Space: 46.5 MB (22.39%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val==0) return false;
+        if(root.val==1) return true;
+        boolean left=evaluateTree(root.left);
+        boolean right=evaluateTree(root.right);
+        if(root.val==2) return left||right;
+        return left && right;
+    }
+}
